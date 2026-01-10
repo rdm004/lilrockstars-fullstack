fbd697583ab2d516c000842e22066a6c5c1024b5
Ryan <ryan.merrill2004@yahoo.com>
2026-01-09 22:38:30 -0500

    Created Shared-Buttons.css to copy facebook button on multiple pages.

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/Contact.css";
+import "../styles/Shared-Buttons.css"
 
 const Contact = () => {
     return (
--- frontend/src/pages/Gallery.js
+++ frontend/src/pages/Gallery.js
@@ -2,6 +2,8 @@
 import React, { useState } from "react";
 import "../styles/Gallery.css";
 import GalleryData from "../data/GalleryData";
+import "../styles/Shared-Buttons.css"
+
 
 const Gallery = () => {
     const [activeImage, setActiveImage] = useState(null);
@@ -10,6 +12,36 @@ const Gallery = () => {
         <div className="gallery-container">
             <h1>📸 Race Gallery 📸</h1>
             <p>Photos from Lil Rockstars Racing events</p>
+
+            <div className="gallery-fb">
+                <p>
+                    For the latest photos check out our Facebook!
+                </p>
+                <a
+                    href="https://www.facebook.com/profile.php?id=100091910351052"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="facebook-button"
+                >
+                    <div className="facebook-icon-container">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="18"
+                            height="18"
+                            fill="currentColor"
+                            viewBox="0 0 24 24"
+                            className="facebook-icon"
+                        >
+                            <path d="M22.675 0h-21.35C.6 0 0 .6 0 1.325v21.351C0 23.4.6 24
+      1.325 24H12.82V14.706h-3.17v-3.622h3.17V8.413c0-3.13 1.91-4.832
+      4.7-4.832 1.34 0 2.493.099 2.829.144v3.28l-1.942.001c-1.524
+      0-1.819.724-1.819 1.787v2.344h3.637l-.474 3.622h-3.163V24h6.203
+      C23.4 24 24 23.4 24 22.675V1.325C24 .6 23.4 0 22.675 0z" />
+                        </svg>
+                    </div>
+                    Lil Rockstars
+                </a>
+            </div>
 
             <div className="event-gallery">
                 <h2>Lil Rockstars</h2>
